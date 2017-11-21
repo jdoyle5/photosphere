45f0fb8112c4390ce2dd1a4dd139f191b75248fc
Joey Doyle <j.doyle345@gmail.com>
2017-11-21 11:38:00 -0800

    Initialize project skeleton

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+// webpack.config.js
+var path = require('path');
+
+module.exports = {
+  entry: './frontend/photosphere.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: './bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
