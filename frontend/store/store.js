abdd43fbf02cdd03639b87d01a9f62101e4556e6
Joey Doyle <j.doyle345@gmail.com>
2017-11-23 10:39:53 -0800

    Refactored the store file to fix heroku push issues

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,11 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-// import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from '../reducers/root';
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
 export default (preloadedState = {}) => createStore(
   rootReducer, preloadedState,
-  applyMiddleware(createLogger(), thunk)
+  composeWithDevTools(applyMiddleware(...middlewares))
 );
