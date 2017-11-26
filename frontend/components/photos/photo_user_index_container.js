7531e66fa60671fa1e95ffd3f29b6f143fef8c2d
Joey Doyle <j.doyle345@gmail.com>
2017-11-25 22:07:43 -0800

    Finished the photo-user-index-container component

---
+++ frontend/components/photos/photo_user_index_container.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+import PhotoUserIndex from './photo_index';
+import { selectPhotos } from '../../reducers/selectors';
+import { requestUserPhotos } from '../../actions/photo.js';
+
+const mapStateToProps = state => ({
+  photos: selectPhotos(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  requestUserPhotos: userId => dispatch(requestUserPhotos(userId))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PhotoUserIndex);
