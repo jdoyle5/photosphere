74e1f84b477cdf1a8b0fa5a2c5d0d3160d119f4b
Joey Doyle <j.doyle345@gmail.com>
2017-12-01 14:54:07 -0800

    Added more comments to my seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -497,7 +497,9 @@ rand_comments = ["This is ridiculous! Great job with the colors.",
   "I really like this one a lot",
   "This makes me want to travel!",
   "So dope!",
-  "Where is this??"]
+  "Where is this??",
+  "Wow I'm so jealous",
+  "So coooooooool"]
 
 
   (1..Photo.count-1).each do |photo_id|
