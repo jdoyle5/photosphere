8344f106044bf27ce8425a2dc7f42072616ddacc
Joey Doyle <j.doyle345@gmail.com>
2017-12-01 10:53:29 -0800

    Edited the package.json, webpack.js and gemfiles to try and fix heroku upload issues.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,36 @@
 // webpack.config.js
 var path = require('path');
+var webpack = require('webpack');
+
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
+
 
 module.exports = {
+  context: __dirname,
   entry: './frontend/photosphere.jsx',
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: './bundle.js',
   },
+  plugins: plugins,
   module: {
     loaders: [
       {
@@ -24,3 +48,32 @@ module.exports = {
     extensions: ['.js', '.jsx', '*']
   }
 };
+
+
+
+// webpack.config.js
+// var path = require('path');
+//
+// module.exports = {
+//   entry: './frontend/photosphere.jsx',
+//   output: {
+//     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+//     filename: './bundle.js',
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         test: [/\.jsx?$/],
+//         exclude: /(node_modules)/,
+//         loader: 'babel-loader',
+//         query: {
+//           presets: ['es2015', 'react']
+//         }
+//       }
+//     ]
+//   },
+//   devtool: 'source-map',
+//   resolve: {
+//     extensions: ['.js', '.jsx', '*']
+//   }
+// };
