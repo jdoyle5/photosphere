9b4f0faa3f67d3e0650ac9583ee5a967ef703c84
Joey Doyle <j.doyle345@gmail.com>
2018-01-06 14:11:04 -0800

    Create Cloudinary Image Upload

--- frontend/components/photos/photo_form.js
+++ frontend/components/photos/photo_form.js
@@ -2,3 +2,6 @@ import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import Dropzone from 'react-dropzone';
 import request from 'superagent';
+
+const CLOUDINARY_UPLOAD_PRESET = 'photosphere_image_upload';
+const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/photosphere/image/upload';
