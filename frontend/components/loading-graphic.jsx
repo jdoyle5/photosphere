414cd389cbf2fb56c9937bd5ed535e9da18f5dda
Joey Doyle <j.doyle345@gmail.com>
2018-01-15 11:49:50 -0800

    Add a loading animation to my components file and css

---
+++ frontend/components/loading-graphic.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const LoadingGraphic = () => (
+  <div class="cssload-wrap">
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  	<div class="cssload-circle"></div>
+  </div>
+);
