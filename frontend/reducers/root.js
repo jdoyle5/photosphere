3dd1024025c72ad417d8e9d4f8f3a099c59b6191
Joey Doyle <j.doyle345@gmail.com>
2018-01-15 16:27:09 -0800

    Add Ui Reducer

--- frontend/reducers/root.js
+++ frontend/reducers/root.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import ErrorsReducer from './errors';
 import SessionReducer from './session.js';
 import EntitiesReducer from './entities';
+import UiReducer from './ui';
 
 const RootReducer = combineReducers({
   entities: EntitiesReducer,
   session: SessionReducer,
-  errors: ErrorsReducer
+  errors: ErrorsReducer,
+  ui: UiReducer
 });
 
 export default RootReducer;
---
+++ frontend/reducers/ui.js
@@ -0,0 +1,26 @@
+import { RECEIVE_PHOTOS,
+         RECEIVE_PHOTO,
+         REMOVE_PHOTO
+} from '../actions/photo';
+import { BEGIN_LOADING } from '../actions/loading';
+
+const _initialState = {
+  loading: false
+};
+
+const UiReducer = (state = _initialState, action) => {
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_PHOTOS:
+      return Object.assign({}, state, { loading: false });
+    case RECEIVE_PHOTO:
+      return Object.assign({}, state, { loading: false });
+    case BEGIN_LOADING:
+      return Object.assign({}, state, { loading: true });
+    default:
+      return state;
+  }
+};
+
+export default UiReducer;
