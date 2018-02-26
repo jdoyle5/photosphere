3ca3d947ebc3c07c851aaff14dd3f639f4e55886
Joey Doyle <j.doyle345@gmail.com>
2018-02-26 12:06:32 -0800

    Take out set timout and console.log in the will mount section

--- frontend/components/photos/photo_user_index.jsx
+++ frontend/components/photos/photo_user_index.jsx
@@ -7,7 +7,6 @@ import { Link } from 'react-router-dom';
 class PhotoUserIndex extends React.Component {
   componentWillMount() {
     this.props.requestUserPhotos(this.props.match.params.userId);
-    console.log("mounted");
   }
 
   componentWillReceiveProps(newProps) {
@@ -22,7 +21,6 @@ class PhotoUserIndex extends React.Component {
 
   render () {
     const { photos, loading } = this.props;
-    setTimeout(function(){ console.log(photos); }, 1000);
     if (loading) {
       return (
         <LoadingGraphic />
@@ -30,14 +28,6 @@ class PhotoUserIndex extends React.Component {
     } else {
       return (
         <div className="index-photo-display">
-          {/* <div className="navbar-user">
-            <div className="navbar-photo-activity">
-              <Link to={`/users/${photos[0].owner_id}/photos`}><img src={ photos[0].user_photo }/></Link>
-            </div>
-            <div className="navbar-username">
-              <Link to={`/users/${photos[0].owner_id}/photos`} className="link-username-activity">{photos[0].username}</Link>
-            </div>
-          </div> */}
           <h2 className="user-activity">Activity</h2>
           <div className="index-sub-display">
             {photos.map( photo =>
