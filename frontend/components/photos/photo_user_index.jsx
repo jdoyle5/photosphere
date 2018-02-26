4d184b068f716cba49c60c66ed81fb8ed199ea08
Joey Doyle <j.doyle345@gmail.com>
2018-02-26 09:42:18 -0800

    Made splash photo https

--- frontend/components/photos/photo_user_index.jsx
+++ frontend/components/photos/photo_user_index.jsx
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 class PhotoUserIndex extends React.Component {
   componentWillMount() {
     this.props.requestUserPhotos(this.props.match.params.userId);
+    console.log("mounted");
   }
 
   componentWillReceiveProps(newProps) {
@@ -21,7 +22,7 @@ class PhotoUserIndex extends React.Component {
 
   render () {
     const { photos, loading } = this.props;
-    const currentUser = this.props.currentUser;
+    setTimeout(function(){ console.log(photos); }, 1000);
     if (loading) {
       return (
         <LoadingGraphic />
@@ -29,14 +30,14 @@ class PhotoUserIndex extends React.Component {
     } else {
       return (
         <div className="index-photo-display">
-          <div className="navbar-user">
+          {/* <div className="navbar-user">
             <div className="navbar-photo-activity">
-              <Link to={`/users/${currentUser.id}/photos`}><img src={ currentUser.img_url }/></Link>
+              <Link to={`/users/${photos[0].owner_id}/photos`}><img src={ photos[0].user_photo }/></Link>
             </div>
             <div className="navbar-username">
-              <Link to={`/users/${currentUser.id}/photos`} className="link-username-activity">{currentUser.username}</Link>
+              <Link to={`/users/${photos[0].owner_id}/photos`} className="link-username-activity">{photos[0].username}</Link>
             </div>
-          </div>
+          </div> */}
           <h2 className="user-activity">Activity</h2>
           <div className="index-sub-display">
             {photos.map( photo =>
