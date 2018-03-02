4cb62bd8700f104580a2be781fd487a29c91ec57
Joey Doyle <j.doyle345@gmail.com>
2018-03-01 16:21:12 -0800

    Add follows actions and utils (ajax calls)

--- frontend/util/follows_api_util.js
+++ frontend/util/follows_api_util.js
@@ -4,3 +4,26 @@ export const fetchFollowers = (userId) => (
     url: `/api/users/${userId}/follows/followers`
   })
 );
+
+export const fetchFollowees = (userId) => (
+  $.ajax({
+    method: 'GET',
+    url: `/api/users/${userId}/follows/followees`
+  })
+);
+
+export const createFollow = (userId, currentUserId) => (
+  $.ajax({
+    method: 'POST',
+    url: `/api/follows`,
+    data: { followee_id: userId, follower_id: currentUserId }
+  })
+);
+
+export const deleteFollow = (userId, currentUserId) => (
+  $.ajax({
+    method: 'DELETE',
+    url: `/api/follows`,
+    data: { followee_id: userId, follower_id: currentUserId }
+  })
+);
